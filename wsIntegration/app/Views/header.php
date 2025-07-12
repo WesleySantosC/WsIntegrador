9174e55442d0dc78e0f1982e09dda7668c594256
Wesley <wesleysantoscardoso2404@gmail.com>
2025-07-12 08:31:59 -0300

    improve: Separando o menu dos arquivos principais

---
+++ wsIntegration/app/Views/header.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="<?= base_url('Styles/StyleHeader.css'); ?>">
+<script>
+    const wwwroot = "<?= base_url() ?>";
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<nav class="navbar">
+    <div class="logo">Ws Integrações</div>
+    <ul class="menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="/plans">Planos</a></li>
+        <li><a href="<?= base_url('contact'); ?>">Contato</a></li>
+        <li><a href="<?= base_url('login'); ?>">Entrar</a></li>
+    </ul>
+</nav>
