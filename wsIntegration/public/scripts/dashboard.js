5ab33ecaf864cf269c9e7a277c4c51ad96508cbb
Wesley <wesleysantoscardoso2404@gmail.com>
2025-08-02 10:46:47 -0300

    improve: rota para ativar imovel

--- wsIntegration/public/scripts/dashboard.js
+++ wsIntegration/public/scripts/dashboard.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
-    const WWWROOT = 'http://localhost:8080/';
-
-    $(".deactive").on("click", function() {
+    $(".deactivate").on("click", function() {
         let idRealty = $(this).data("id");
 
         $.post(
-            WWWROOT + 'dashboard/desativaImovel',
+            wwwroot + 'dashboard/desativaImovel',
             { id: idRealty },
             function(result) {
-                console.log(result);
                 if(result.status === 'success') {
                     Swal.fire({
                         title: 'Imóvel Desativado!',
                         text: 'O imóvel foi desativado!',
                         icon: 'success'
                     }).then(() => {
-                        window.location.href=WWWROOT + 'dashboard'
+                        window.location.href=wwwroot + 'dashboard'
                     });
                 } else {
                     Swal.fire({
@@ -29,9 +26,43 @@ $(document).ready(function() {
         );
     });
 
+    $(".active").click(function() {
+        let realtyId = $(this).data("id");
+
+        $.post(
+            wwwroot + 'dashboard/activeRealty',
+            { id: realtyId },
+            function(result) {
+                if(result.status == 'success') {
+                    Swal.fire({
+                        title: 'Imóvel Ativado!',
+                        text : 'Imóvel ativado com sucesso!',
+                        icon : 'success'
+                    }).then(() => {
+                        window.location.href=wwwroot + 'dashboard'
+                    });;
+                } else {
+                        Swal.fire({
+                        title: 'Error',
+                        text : result.error,
+                        icon : 'error'
+                    });
+                }
+            }
+        );
+    });
+
+    $("#openModal").click(function() {
+        $("#modal").show();
+    });
+
+    $("#close").click(function() {
+        $("#modal").hide();
+    });
+
     $("#logout").on("click", function() {
         $.post(
-            WWWROOT + 'login/logout', {}, function() {
+            wwwroot + 'login/logout', {}, function() {
                 console.log("Sessão Destruida");
             }
         )
