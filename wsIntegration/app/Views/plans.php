2a5559d77b4f7a9a295cb9753a06db51a7b96d39
Wesley <wesleysantoscardoso2404@gmail.com>
2025-09-02 17:07:56 -0300

    chore: Alterando algumas coisas para deixar gratuito no momento

--- wsIntegration/app/Views/plans.php
+++ wsIntegration/app/Views/plans.php
@@ -7,11 +7,11 @@
         <?php foreach ($plans as $plan): ?>
             <div class="plan-card <?= $plan->is_featured ? 'featured' : '' ?>">
                 <h2><?= $plan->name ?></h2>
-                <p><strong>Preço:</strong> R$ <?= number_format($plan->price, 2, ',', '.') ?></p>
-                <p><strong>Duração:</strong> <?= $plan->duration_days ?> dias</p>
-                <p><strong>Anúncios:</strong> <?= $plan->qtd_anuncio ?></p>
-                <p><strong>Destaques:</strong> <?= $plan->qtd_destaque ?></p>
-                <p><strong>Destaque permitido?</strong> <?= $plan->is_featured ? 'Sim' : 'Não' ?></p>
+                <p><strong>Preço:</strong><?= !$plan->price ? "R$ " . number_format($plan->price, 2, ',', '.') : ' Gratuito'?></p>
+                <p><strong>Duração:</strong> <?= "∞" //$plan->duration_days ?></p>
+                <p><strong>Anúncios:</strong> <?= "∞" // $plan->qtd_anuncio ?></p>
+                <!--<p><strong>Destaques:</strong> <?php //$plan->qtd_destaque ?></p> -->
+                <!--<p><strong>Destaque permitido?</strong> <?php //$plan->is_featured ? 'Sim' : 'Não' ?></p>-->
                 <button class="btn-escolher" onclick="location.href='<?= base_url('payment/' . $plan->id) ?>'">Escolher</button>
                 </div>
         <?php endforeach; ?>
