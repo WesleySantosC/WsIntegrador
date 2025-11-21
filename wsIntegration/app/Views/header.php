203bd6230496b6e7dabd05b03432ecf917944418
Wesley <wesleysantoscardoso2404@gmail.com>
2025-11-21 17:41:47 -0300

    fix: Ajuste no nome

--- wsIntegration/app/Views/header.php
+++ wsIntegration/app/Views/header.php
@@ -5,7 +5,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <nav class="navbar">
-    <div class="logo">Ws Integrações</div>
+    <div class="logo">Movy</div>
     <ul class="menu">
         <li><a href="/">Home</a></li>
         <li><a href="/plans">Planos</a></li>
