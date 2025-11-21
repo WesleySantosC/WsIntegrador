7af2c22a93384e22ab7cda0ce21425badeb3875e
Wesley <wesleysantoscardoso2404@gmail.com>
2025-11-21 17:41:35 -0300

    fix: Ajuste no nome

--- wsIntegration/app/Views/footer.php
+++ wsIntegration/app/Views/footer.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="<?= base_url('Styles/StyleFooter.css'); ?>">
 
 <footer class="footer">
-    <p>&copy; 2025 Ws Integrações - Todos os direitos reservados.</p>
+    <p>&copy; 2025 Movy - Todos os direitos reservados.</p>
 </footer>
