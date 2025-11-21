4f238e1704ad6aff52ccf658bba0b292f1734c02
Wesley <wesleysantoscardoso2404@gmail.com>
2025-11-21 17:46:12 -0300

    feat: Adicionando a variavel wwwroot

--- wsIntegration/public/scripts/RegisterNewPassword.js
+++ wsIntegration/public/scripts/RegisterNewPassword.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     const ROUTE   = "resetPassword";
     let form      = $("#frmz_resetPassword");
+    let wwwroot   = 'http://localhost:8080/';
 
     form.submit(function(e) {
         e.preventDefault();
@@ -11,7 +12,13 @@ $(document).ready(function() {
             wwwroot + ROUTE, formSerialize, function(response) {
                 console.log(response);
                 if(response.status === 'success'){
-                    Swal.fire('Sucesso', response.message, 'success');
+                    Swal.fire(
+                        'Sucesso', 
+                        response.message, 
+                        'success'
+                    ).then(function() {
+                        window.location.href= wwwroot + "/login";
+                    });
                     form[0].reset();
                 } else {
                     Swal.fire('Erro', response.message, 'error');
