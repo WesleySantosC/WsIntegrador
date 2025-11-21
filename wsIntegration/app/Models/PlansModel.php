9f2bc663348fc6883a7245d90de827bc9ae94c99
Wesley <wesleysantoscardoso2404@gmail.com>
2025-11-21 17:37:21 -0300

    feat: Pegando o plano que o cliente escolheu

--- wsIntegration/app/Models/PlansModel.php
+++ wsIntegration/app/Models/PlansModel.php
@@ -9,7 +9,16 @@ class PlansModel extends Model
     protected $table            = 'plans';
     protected $primaryKey       = 'id';
 
-    protected $allowedFields = ['id', 'name', 'price', 'duration_days', 'max_properties', 'qtd_anuncio', 'qtd_destaque', 'is_featured', 'created_at'];
+    protected $allowedFields = ['id', 
+                                'name', 
+                                'price', 
+                                'duration_days', 
+                                'max_properties', 
+                                'qtd_anuncio', 
+                                'qtd_destaque', 
+                                'is_featured', 
+                                'created_at'
+                            ];
 
     public function getPlans() {
         $builder = $this->db->table($this->table . " p");
@@ -17,5 +26,18 @@ class PlansModel extends Model
         $builder->where("p.visible_plan", 1);
 
         return $builder->get()->getResult();
+    }
+
+    public function obtainThePlanChosenByTheCustomer($idUser) {
+        $builder = $this->db->table($this->table . " p");
+        $builder->select("
+                          p.qtd_anuncio,
+                          p.qtd_destaque"
+                        );
+        $builder->join("subscription_types st", "st.id_plan = p.id", "inner");
+        $builder->join("usuarios u", "u.cpf_cnpj = st.cpf_cnpj_user", "inner");
+        $builder->where("u.id", $idUser);
+
+        return $this->getRow($builder);
     }
 }
