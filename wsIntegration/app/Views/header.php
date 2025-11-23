d2b6aa5ec1c507a8a8b5b59cd09be77fb80b066c
Wesley <wesleysantoscardoso2404@gmail.com>
2025-11-23 12:20:42 -0300

    Style: Ajuste para mobile

--- wsIntegration/app/Views/header.php
+++ wsIntegration/app/Views/header.php
@@ -4,8 +4,13 @@
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <nav class="navbar">
-    <div class="logo">Movy</div>
+    <div class="logo">
+        <a href="<?= base_url("/"); ?>">
+        <img src="<?= base_url("imgs/logo_movy.png"); ?>" style="max-width: 20%;" alt="Logo">
+        </a>
+    </div>
     <ul class="menu">
         <li><a href="/">Home</a></li>
         <li><a href="/plans">Planos</a></li>
--- wsIntegration/app/Views/headerAdminClient.php
+++ wsIntegration/app/Views/headerAdminClient.php
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="<?= base_url('Styles/StyleHeaderAdminClient.css'); ?>">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -5,21 +6,50 @@
 <script>
     const wwwroot = "<?= base_url() ?>";
 </script>
+
+<div class="hamburger" id="hamburger">&#9776;</div><br><br><br>
+
 <div class="sidebar">
     <div class="logo">
-        <h1>Movy</h1>
+        <a href="<?= base_url("index.php/dashboard"); ?>">
+            <img src="<?= base_url("/imgs/logo_movy.png"); ?>" alt="Logo" class="logo_img">
+        </a>
     </div>
+    
     <div class="greeting">
         Olá, <?php if (isset($infoClients[0])) { $nome = $infoClients[0]['nome']; } else { $nome = $infoClients['nome']; } echo $nome;?>
     </div>
+
     <nav class="menu">
         <h2>Menu</h2>
         <ul class="menu-list">
-            <li><a href="<?= site_url('/dashboard') ?>" class="menu-item"><i class="fas fa-database"></i> Home</a></li>
-            <li><a href="<?= site_url('/dataClient') ?>" class="menu-item"><i class="fas fa-database"></i> Dados</a></li>
-            <li><a href="<?= site_url('/cadastrarImovel') ?>" class="menu-item"><i class="fas fa-stethoscope"></i>Cadastrar Imoveis</a></li>
-            <li><a href="<?= site_url('/generateLinkXml') ?>" class="menu-item"><i class="fas fa-stethoscope"></i>Gerar XML</a></li>
-            <li><a href="<?= site_url('/') ?>" class="menu-item" id="logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+            <li><a href="<?= site_url('/dashboard') ?>" class="menu-item">Home</a></li>
+            <li><a href="<?= site_url('/dataClient') ?>" class="menu-item">Dados</a></li>
+            <li><a href="<?= site_url('/cadastrarImovel') ?>" class="menu-item">Cadastrar Imóveis</a></li>
+            <li><a href="<?= site_url('/generateLinkXml') ?>" class="menu-item">Gerar XML</a></li>
+            <li><a href="<?= site_url('/') ?>" class="menu-item" id="logout">Logout</a></li>
         </ul>
     </nav>
 </div>
+
+<script>
+$(document).ready(function () {
+    $("#hamburger").click(function () {
+        $(".sidebar").toggleClass("active");
+
+        let atual = $(this).text().trim();
+
+        if (atual === "☰") {
+            $(this).text("X");
+        } else {
+            $(this).text("☰");
+        }
+    });
+
+    $(".menu-item").click(() => {
+        $(".sidebar").removeClass("active");
+        $("#hamburger").text("☰");
+    });
+});
+
+</script>
