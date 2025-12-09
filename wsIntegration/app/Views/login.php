7bb4e59fd9003b143beb27db0f284ebc919e3cc0
Wesley <wesleysantoscardoso2404@gmail.com>
2025-12-09 10:25:21 -0300

    fix: Pequenas alterações

--- wsIntegration/app/Views/login.php
+++ wsIntegration/app/Views/login.php
@@ -1,12 +1,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="<?= base_url('Styles/styleLogin.css'); ?>">
+<link rel="icon" href="<?= base_url('imgs/fivecon_muvy.png') ?>">
 
 <title>Login</title>
 
-<h1>Login</h1>
 <form action="<?= site_url('login/verifyUsers') ?>" method="post">
+    <h1>Login</h1>
     <label for="email">E-mail:</label>
-    <input type="email" id="email" name="email" placeholder="Ex: Seu E-mail" autocomplete="off" required>
+    <input type="email" id="email" name="email" placeholder="Seu E-mail" autocomplete="off" required>
     <br><br>
 
     <label for="password">Senha:</label>
